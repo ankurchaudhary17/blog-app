3da59028877074efc9c7ad89cfe0c157b53667be
Vivek Singh <vivek.singh@codingninjas.com>
2024-06-17 04:14:25 +0530

    Added firebase as a storage

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -1,85 +1,132 @@
-import React ,{useState,useContext,useEffect}from 'react'
+import React, { useState, useContext, useEffect } from 'react'
 import ReactQuill from 'react-quill'
 import 'react-quill/dist/quill.snow.css'
 import { UserContext } from '../context/userContext'
-import {  useNavigate } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
+import {
+  getStorage,
+  ref,
+  uploadBytesResumable,
+  getDownloadURL,
+} from "firebase/storage";
+import app from '../firebase'
 import axios from 'axios'
 
 const CreatePost = () => {
-  const [title,settitle]=useState('')
-  const [category,setCategory]=useState('Uncategorized')
-  const [description,setDescription]=useState('')
-  const [thumbnail,setThumbnail]=useState('')
-  const [error,setError]=useState('')
+  const [title, settitle] = useState('')
+  const [category, setCategory] = useState('Uncategorized')
+  const [description, setDescription] = useState('')
+  const [thumbnail, setThumbnail] = useState(undefined)
+  const [thumbnailUrl, setThumbnailUrl] = useState('')
+  const [error, setError] = useState('')
 
-  const navigate=useNavigate();
-  const {currentUser}=useContext(UserContext)
-    const token=currentUser?.token;
+  const navigate = useNavigate();
+  const { currentUser } = useContext(UserContext)
+  const token = currentUser?.token;
 
-    //redirect to login page for any user who is not logged in
-    useEffect(()=>{
-      if(!token){
-        navigate('/login')
-      }
-    },[])
+  //redirect to login page for any user who is not logged in
+  useEffect(() => {
+    if (!token) {
+      navigate('/login')
+    }
+  }, [])
 
-  const modules={
-    toolbar:[
-      [{'header':[1,2,3,4,5,6,false]}],
-      ['bold','italic','underline','strike','blockquote'],
-      [{'list': 'ordered'},{'list':'bullet'},{'indent':'-1'},{'indent':'+1'}],
-      ['link','image'],
+  const modules = {
+    toolbar: [
+      [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+      ['bold', 'italic', 'underline', 'strike', 'blockquote'],
+      [{ 'list': 'ordered' }, { 'list': 'bullet' }, { 'indent': '-1' }, { 'indent': '+1' }],
+      ['link', 'image'],
       ['clean']
     ],
   }
 
-  const formats=[
+  const formats = [
     'header',
-    'bold','italic','underline','strike','blockquote',
-    'list','bullet','indent',
-    'link','image'
+    'bold', 'italic', 'underline', 'strike', 'blockquote',
+    'list', 'bullet', 'indent',
+    'link', 'image'
   ]
 
-  const POST_CATEGORIES=["Agriculture","Business","Education","Entertainment","Art","Investment","Uncategorized","Weather"]
+  const POST_CATEGORIES = ["Agriculture", "Business", "Education", "Entertainment", "Art", "Investment", "Uncategorized", "Weather"]
 
-  const createPost=async(event)=>{
-   event.preventDefault();
-   const postData=new FormData();
-   postData.set('title',title)
-   postData.set('category',category)
-   postData.set('description',description)
-   postData.set('thumbnail',thumbnail)
-   
-   try{
-  const response=await axios.post(`${process.env.REACT_APP_BASE_URL}/posts`,postData,{withCredentials:true,headers:{Authorization:`Bearer ${token}`}})
-  if(response.status===201){
-    return navigate('/')
+  const uploadFile = (file,urlType) => {
+    const storage = getStorage(app);
+    console.log("ab");
+    const fileName = new Date().getTime() + file?.name;
+    const storageRef = ref(storage, fileName);
+    const uploadTask = uploadBytesResumable(storageRef, file);
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        // Observe state change events such as progress, pause, and resume
+        // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+        const progress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        console.log("Upload is " + progress + "% done");
+        // urlType==='imgUrl'?setImgPerc(Math.round(progress)):setVideoPerc(Math.round(progress))
+        switch (snapshot.state) {
+          case "paused":
+            console.log("Upload is paused");
+            break;
+          case "running":
+            console.log("Upload is running");
+            break;
+          default:
+            break;
+        }
+      },
+      (error) => {
+        // Handle unsuccessful uploads
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+          setThumbnailUrl(downloadURL);
+        });
+      }
+    );
+  };
+
+  const createPost = async (event) => {
+    event.preventDefault();
+    const postData = new FormData();
+    postData.set('title', title)
+    postData.set('category', category)
+    postData.set('description', description)
+    postData.set('thumbnail', thumbnailUrl)
+
+    try {
+      const response = await axios.post(`${process.env.REACT_APP_BASE_URL}/posts`, postData, { withCredentials: true, headers: { Authorization: `Bearer ${token}` } })
+      if (response.status === 201) {
+        return navigate('/')
+      }
+    } catch (err) {
+      setError(err.response.data.message)
+    }
   }
-   }catch(err){
-    setError(err.response.data.message)
-    
-   }
-  }
+  useEffect(() => {
+    thumbnail && uploadFile(thumbnail,"thumbnailUrl");
+  }, [thumbnail]);
 
 
   return (
-   <section className='create-post'>
-    <div className='container'>
-      <h2>Create Post</h2>
-     {error && <p className='form_error-message'>{error}</p>}
-    </div>
-    <form className='form create-post_form' onSubmit={createPost}>
-      <input type='text' placeholder='Title' value={title} onChange={e=>settitle(e.target.value)} autoFocus/>
-      <select name='category' value={category} onChange={e=>setCategory(e.target.value)}>
-        {
-          POST_CATEGORIES.map(cat=> <option key={cat}>{cat}</option>)
-        }
-      </select>
-        <ReactQuill modules={modules} formats={formats} value={description} onChange={setDescription}/>
-        <input type='file' onChange={e=>setThumbnail(e.target.files[0])} accept='png , jpg,jpeg'/>
+    <section className='create-post'>
+      <div className='container'>
+        <h2>Create Post</h2>
+        {error && <p className='form_error-message'>{error}</p>}
+      </div>
+      <form className='form create-post_form' onSubmit={createPost}>
+        <input type='text' placeholder='Title' value={title} onChange={e => settitle(e.target.value)} autoFocus />
+        <select name='category' value={category} onChange={e => setCategory(e.target.value)}>
+          {
+            POST_CATEGORIES.map(cat => <option key={cat}>{cat}</option>)
+          }
+        </select>
+        <ReactQuill modules={modules} formats={formats} value={description} onChange={setDescription} />
+        <input type='file' onChange={e => setThumbnail(e.target.files[0])} accept='png , jpg,jpeg' />
         <button type='submit' className='btn primary'>Create</button>
-    </form>
-   </section>
+      </form>
+    </section>
   )
 }
 
--- server/controllers/postControllers.js
+++ server/controllers/postControllers.js
@@ -14,50 +14,28 @@ const { v4: uuid } = require("uuid");
 
 const createPost = async (req, res, next) => {
   try {
-    let { title, category, description } = req.body;
-    if (!title || !category || !description || !req.files) {
+    let { title, category, description, thumbnail } = req.body;
+    console.log(title,category,description,thumbnail);
+    if (!title || !category || !description || !thumbnail) {
       return next(
         new HttpError("Fill in all fields and choose thumbnail.", 422)
       );
     }
-    const { thumbnail } = req.files;
-    //check the file size
-    if (thumbnail.size > 2000000) {
-      return next(
-        new HttpError("Thumbnail too big . File should be less than 2mb.")
-      );
+    const newPost = await Post.create({
+      title,
+      category,
+      description,
+      thumbnail,
+      creator: req.user.id,
+    });
+    if (!newPost) {
+      return next(new HttpError("Post could not created", 422));
     }
-    let fileName = thumbnail.name;
-    let splittedFilename = fileName.split(".");
-    let newFilename =
-      splittedFilename[0] +
-      uuid() +
-      "." +
-      splittedFilename[splittedFilename.length - 1];
-    thumbnail.mv(
-      path.join(__dirname, "..", "/uploads", newFilename),
-      async (err) => {
-        if (err) {
-          return next(new HttpError(err));
-        } else {
-          const newPost = await Post.create({
-            title,
-            category,
-            description,
-            thumbnail: newFilename,
-            creator: req.user.id,
-          });
-          if (!newPost) {
-            return next(new HttpError("Post could not created", 422));
-          }
-          // find user and increase post count by one
-          const currentUser = await User.findById(req.user.id);
-          const userPostCount = currentUser.posts + 1;
-          await User.findByIdAndUpdate(req.user.id, { posts: userPostCount });
-          res.status(201).json(newPost);
-        }
-      }
-    );
+    // find user and increase post count by one
+    const currentUser = await User.findById(req.user.id);
+    const userPostCount = currentUser.posts + 1;
+    await User.findByIdAndUpdate(req.user.id, { posts: userPostCount });
+    res.status(201).json(newPost);
   } catch (error) {
     return next(new HttpError(error));
   }
